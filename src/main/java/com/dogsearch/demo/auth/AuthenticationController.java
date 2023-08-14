fd19e225f540521641a7f8144ecd054c48284aac
BrunoPanassi <bhenrique95@outlook.com>
2023-08-14 08:09:43 -0300

    add cross origin to AuthenticationController

--- src/main/java/com/dogsearch/demo/auth/AuthenticationController.java
+++ src/main/java/com/dogsearch/demo/auth/AuthenticationController.java
@@ -3,13 +3,11 @@ package com.dogsearch.demo.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
